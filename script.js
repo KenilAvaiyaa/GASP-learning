5377cf25b029650156162252674221242da1177a
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-02-29 14:32:15 +0530

    fix animation

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ const time = gsap.timeline({
 time.to(".images", {
         width:"100%",
         ease : Expo.easeInOut,
-        duration: 2,
-        stagger:2
+        duration: 2 ,
+        stagger: 2
     },"a")
 .to(".text h1", {
     top: "0",
+    delay: 0.9,
     ease : Expo.easeInOut,
     stagger: 2
 },"a")
 .to(".text h1", {
+    delay: 2,
     top: "-100%",
     ease : Expo.easeInOut,
-    stagger: 2,
-    delay: 2
+    stagger: 2
 },"a")
